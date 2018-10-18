da03fac3d29ab9edddac3615c445be9e57ca2f9d
dongdong <286084845@qq.com>
2018-10-18 23:29:25 +0800

    首页文件

---
+++ index.js
@@ -0,0 +1,62 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 8888;
+
+var server = http.createServer(function(request, response){
+
+  var temp = url.parse(request.url, true)
+  var path = temp.pathname
+  var query = temp.query
+  var method = request.method
+
+  //从这里开始看，上面不要看
+
+  if(path === '/'){  // 如果用户请求的是 / 路径
+    var string = fs.readFileSync('./index.html', 'utf8')  
+    var amount = fs.readFileSync('./db.txt', 'utf8')
+    string = string.replace('&&&amount&&&', amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')  
+    response.write(string)   
+    response.end()
+  }else if(path === '/style.css'){   
+    var string = fs.readFileSync('./style.css', 'utf8')
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){  
+    var string = fs.readFileSync('./main.js', 'utf8')
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write(string)
+    response.end()
+  }else if (path === '/pay') {
+    var amount = fs.readFileSync('./db.txt', 'utf8')
+    var newAmount = amount - 1
+    if (Math.random() > 0.5) {
+      fs.writeFileSync('./db.txt', newAmount) 
+      response.setHeader('Content-Type', 'application/javascript')
+      response.statusCode = 200
+      response.write(`
+        amount.innerText = amount.innerText - 1
+        `)
+      response.end()
+    } else {
+      response.statusCode = 400
+      response.write('failed')
+      response.end()    
+    }
+  } else {  
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8') 
+    response.write('找不到对应的路径，你需要自行修改 index.js')
+    response.end()
+  }
+
+  // 代码结束，下面不要看
+  console.log(method + ' ' + request.url)
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功，请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
